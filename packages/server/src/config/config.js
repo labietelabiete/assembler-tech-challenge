41a6cee295b75af11c4edb0141a95774fd5baf3b
labietelabiete <victorhugogote@gmail.com>
2021-11-03 18:12:39 +0100

    Feat: MongoAtlas set up database

--- packages/server/src/config/config.js
+++ packages/server/src/config/config.js
@@ -2,78 +2,94 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+// const {
+//   NODE_ENV = "production",
+//   MONGO_DB_URL_PRODUCTION,
+//   MONGO_DB_URL_DEVELOPMENT,
+//   MONGO_DB_URL_TEST,
+//   // ACCESS_TOKEN_SECRET,
+//   PORT,
+//   ENCRYPTION_SALT_DEVELOPMENT,
+//   ENCRYPTION_SALT_PRODUCTION,
+//   CLOUDINARY_API_KEY,
+//   CLOUDINARY_API_SECRET,
+//   CLOUDINARY_NAME,
+// } = process.env;
+
+// const CONFIG = {
+//   production: {
+//     app: {
+//       port: PORT || 4000,
+//     },
+//     db: {
+//       url: MONGO_DB_URL_PRODUCTION,
+//     },
+//     encrypt: {
+//       salt: ENCRYPTION_SALT_PRODUCTION,
+//     },
+//     cloudinary: {
+//       key: CLOUDINARY_API_KEY,
+//       secret: CLOUDINARY_API_SECRET,
+//       name: CLOUDINARY_NAME,
+//     },
+//   },
+//   development: {
+//     app: {
+//       port: PORT || 4000,
+//     },
+//     db: {
+//       url: MONGO_DB_URL_DEVELOPMENT,
+//     },
+//     encrypt: {
+//       salt: ENCRYPTION_SALT_DEVELOPMENT,
+//     },
+//     cloudinary: {
+//       key: CLOUDINARY_API_KEY,
+//       secret: CLOUDINARY_API_SECRET,
+//       name: CLOUDINARY_NAME,
+//     },
+//   },
+//   test: {
+//     app: {
+//       port: PORT || 4000,
+//     },
+//     db: {
+//       url: MONGO_DB_URL_TEST,
+//     },
+//     cloudinary: {
+//       key: CLOUDINARY_API_KEY,
+//       secret: CLOUDINARY_API_SECRET,
+//       name: CLOUDINARY_NAME,
+//     },
+//   },
+// };
+
 const {
-  NODE_ENV = "production",
-  MONGO_DB_URL_PRODUCTION,
-  MONGO_DB_URL_DEVELOPMENT,
-  MONGO_DB_URL_TEST,
-  // ACCESS_TOKEN_SECRET,
+  MONGO_DB_URL,
   PORT,
-  ENCRYPTION_SALT_DEVELOPMENT,
-  ENCRYPTION_SALT_PRODUCTION,
+  // GIPHY_API_KEY,
   CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET,
   CLOUDINARY_NAME,
-  WAVE_STATS_API_KEY,
 } = process.env;
 
 const CONFIG = {
-  production: {
-    app: {
-      port: PORT || 4000,
-    },
-    db: {
-      url: MONGO_DB_URL_PRODUCTION,
-    },
-    encrypt: {
-      salt: ENCRYPTION_SALT_PRODUCTION,
-    },
-    cloudinary: {
-      key: CLOUDINARY_API_KEY,
-      secret: CLOUDINARY_API_SECRET,
-      name: CLOUDINARY_NAME,
-    },
-    statsServer: {
-      key: WAVE_STATS_API_KEY,
-    },
+  app: {
+    port: PORT || 4000,
   },
-  development: {
-    app: {
-      port: PORT || 4000,
-    },
-    db: {
-      url: MONGO_DB_URL_DEVELOPMENT,
-    },
-    encrypt: {
-      salt: ENCRYPTION_SALT_DEVELOPMENT,
-    },
-    cloudinary: {
-      key: CLOUDINARY_API_KEY,
-      secret: CLOUDINARY_API_SECRET,
-      name: CLOUDINARY_NAME,
-    },
-    statsServer: {
-      key: WAVE_STATS_API_KEY,
-    },
+  db: {
+    url: MONGO_DB_URL,
   },
-  test: {
-    app: {
-      port: PORT || 4000,
-    },
-    db: {
-      url: MONGO_DB_URL_TEST,
-    },
-    cloudinary: {
-      key: CLOUDINARY_API_KEY,
-      secret: CLOUDINARY_API_SECRET,
-      name: CLOUDINARY_NAME,
-    },
-    statsServer: {
-      key: WAVE_STATS_API_KEY,
-    },
+  cloudinary: {
+    key: CLOUDINARY_API_KEY,
+    secret: CLOUDINARY_API_SECRET,
+    name: CLOUDINARY_NAME,
   },
+  // giphy: {
+  //   apiKey: GIPHY_API_KEY,
+  // },
 };
 
 module.exports = {
-  config: CONFIG[NODE_ENV],
+  config: CONFIG,
 };
